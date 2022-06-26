1a473c195026ed754f9cb254cd6715bc44321b8b
xnought <bertuccd@oregonstate.edu>
2022-06-25 22:43:06 -0400

    feat: combinded legend and scatter into one api

--- frontend/src/legend/Legend.svelte
+++ frontend/src/legend/Legend.svelte
@@ -1,20 +1,27 @@
 <script lang="ts">
 	import DiscreteLegend from "./DiscreteLegend.svelte";
+	export let data = [
+		{ color: "red", value: "1" },
+		{ color: "blue", value: "2" },
+	];
+	export let gap = 0.2;
+	export let squareWidth = 10;
+	export let height = 250;
 </script>
 
 <div id="legend">
 	<div id="legend-title">Color Legend</div>
 	<div id="legend-actual">
-		<DiscreteLegend />
+		<DiscreteLegend {data} width={100} {gap} {squareWidth} {height} />
 	</div>
 </div>
 
 <style lang="scss">
 	#legend {
 		border-radius: 3px;
-		background-color: hsla(0, 0%, 100%, 0.15);
+		background-color: hsla(0, 0%, 100%, 0.5);
 		border: 1px solid hsla(0, 0%, 0%, 0.05);
-		backdrop-filter: blur(5px);
+		backdrop-filter: blur(30px);
 		display: inline-block;
 		padding: 10px;
 
---
+++ frontend/src/scatter/LegendaryScatter.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import Legend from "../legend/Legend.svelte";
+	import AutoscaledRegl from "./AutoscaledRegl.svelte";
+	import * as d3 from "d3";
+
+	const defaultColors = d3.schemeCategory10 as string[];
+	export let width = 800;
+	export let height = 800;
+	export let points = new Array(10_000).fill(0).map((_, i) => ({
+		color: Math.random(),
+		opacity: 0.65,
+		x: (Math.PI * i) / 2,
+		y: Math.sin(i),
+	}));
+	export let legend = defaultColors.map((c, i) => ({
+		color: c,
+		value: `${i}`,
+	}));
+	$: colorRange = legend.map((item) => item.color);
+</script>
+
+<div id="legendary" style:width="{width}px" style:height="{height}px">
+	<div id="top-legend">
+		<AutoscaledRegl {width} {height} {colorRange} {points} />
+	</div>
+	<div id="bottom-scatter">
+		<Legend gap={5} data={legend} squareWidth={25} />
+	</div>
+</div>
+
+<style lang="scss">
+	#legendary {
+		position: relative;
+		border: 1px solid lightgray;
+		#top-legend {
+			position: absolute;
+			left: 0;
+			top: 0;
+		}
+	}
+</style>
