3b779d57c2442e2ce10e80f30637f5af73c7f819
Alex Cabrera <alex.cabrera@gmail.com>
2022-07-10 11:33:46 -0400

    feat: audio transcription

--- views/template/{{cookiecutter.folder_name}}/frontend/rollup.config.js
+++ views/template/{{cookiecutter.folder_name}}/frontend/rollup.config.js
@@ -1,9 +1,10 @@
 import svelte from "rollup-plugin-svelte";
+import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import pkg from "./package.json";
 
 export default {
   input: "src/index.js",
   output: [{ file: pkg.module, format: "es" }],
-  plugins: [svelte({ emitCss: false }), resolve()],
+  plugins: [commonjs(), svelte({ emitCss: false }), resolve()],
 };
