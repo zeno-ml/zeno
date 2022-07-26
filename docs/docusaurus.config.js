67d2f72a7a7c3c07eaa7dbdafe95cc2d54407873
Alex Cabrera <alex.cabrera@gmail.com>
2022-07-26 18:54:19 -0400

    fix: base URL

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: "Zeno",
   tagline: "Machine Learning Evaluation Framework",
   url: "https://zenoml.com",
-  baseUrl: "/",
+  baseUrl: "/zeno/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.png",
