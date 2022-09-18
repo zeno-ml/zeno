07b0ad1e897ed0707fd26216c57ecda8dfc17429
xnought <bertuccd@oregonstate.edu>
2022-09-17 19:52:59 -0700

    feat: pickle file cache for mirror

--- zeno/mirror/cache.py
+++ zeno/mirror/cache.py
@@ -1,79 +1,51 @@
 import os
 import pickle
-from typing import Callable
+from abc import ABC, abstractmethod
 
 
-def blank_func():
-    return None
+class MemoryFormat(ABC):
+    @abstractmethod
+    def get(self):
+        pass
+
+    @abstractmethod
+    def set(self, value):
+        pass
+
+    @abstractmethod
+    def exists(self):
+        pass
+
+    @abstractmethod
+    def free(self):
+        pass
 
 
-class ValueCache:
-    def __init__(self, path: str, name: str, internal_memory=True, file_memory=True):
+class PickleMemory(MemoryFormat):
+    def __init__(self, path: str, name: str):
         self.path = path
         self.name = f"{name}.pickle"
         self.file_ptr = os.path.join(self.path, self.name)
-        self.internal_memory = internal_memory
-        self.file_memory = file_memory
 
-        self.memory = None
-
-    @property
-    def file_exists(self):
-        return os.path.exists(self.file_ptr)
-
-    @property
-    def internal_exists(self):
-        return self.memory is not None
-
-    def file_remove(self):
-        if self.file_exists:
-            os.remove(self.file_ptr)
-
-    def internal_remove(self):
-        if self.internal_exists:
-            self.memory = None
-
-    def remove(self):
-        self.file_remove()
-        self.internal_remove()
-
-    def file_set(self, value):
-        if self.file_memory:
-            with open(self.file_ptr, "wb") as f:
-                pickle.dump(value, f)
-
-    def internal_set(self, value):
-        if self.internal_memory:
-            self.memory = value
+    def get(self):
+        if self.exists():
+            with open(self.file_ptr, "rb") as f:
+                return pickle.load(f)
+        else:
+            return None
 
     def set(self, value):
-        self.file_set(value)
-        self.internal_set(value)
+        with open(self.file_ptr, "wb") as f:
+            pickle.dump(value, f)
 
-    def file_get(self):
-        if self.file_exists:
-            with open(self.file_ptr, "rb") as f:
-                value = pickle.load(f)
-                if self.internal_memory:
-                    self.memory = value
-                return value
-        else:
-            return None
+    def exists(self):
+        return os.path.exists(self.file_ptr)
 
-    def internal_get(self):
-        if self.internal_exists:
-            return self.memory
-        else:
-            return None
+    def free(self):
+        if self.exists():
+            os.remove(self.file_ptr)
 
-    def get(self, default_func: Callable = blank_func):
-        # first two check for if in cache
-        if self.internal_exists:
-            return self.internal_get()
-        elif self.file_exists:
-            return self.file_get()
-        else:  # if not in cache, run the default
-            # hard save to memory
-            value = default_func()
-            self.set(value)
-            return value
+
+class ValueCache(PickleMemory):
+    def __init__(self, path: str, name: str):
+        super().__init__(path, name)
--- zeno/mirror/mirror.py
+++ zeno/mirror/mirror.py
@@ -14,10 +14,10 @@ def umap(*args, **kwargs):
 class Mirror:
     def __init__(self, df: DataFrame, cache_path):
         self.df = df
-        self.cache = ValueCache(cache_path, name="projection")
+        self.cache = ValueCache(path=cache_path, name="mirror_projection")
 
     def project(self, model: str):
-        def _umap():
+        if not self.cache.exists():
             # extract data
             embed_col = ZenoColumn(column_type=ZenoColumnType.EMBEDDING, name=model)
             embed = self.df[str(embed_col)].tolist()
@@ -25,8 +25,7 @@ class Mirror:
             # reduce high dim -> low dim
             reducer = umap()
             projection = reducer.fit_transform(embed).tolist()
+            self.cache.set(projection)
             return projection
-
-        # if not found will run the umap
-        value = self.cache.get(default_func=_umap)
-        return value
+        else:
+            return self.cache.get()
