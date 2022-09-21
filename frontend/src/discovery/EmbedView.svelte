5e7fc95053dd0a868f9c6a72e9654c7982157866
xnought <bertuccd@oregonstate.edu>
2022-09-20 19:35:49 -0700

    feat: interactions for UMAP filter + reproject

--- frontend/src/discovery/EmbedView.svelte
+++ frontend/src/discovery/EmbedView.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Scatter from "./scatter/Scatter.svelte";
+	import Button from "@smui/button";
 	import { colorSpec } from "../stores";
 	import type { ScatterRowsWithIds } from "./scatter/scatter";
 	import { onMount } from "svelte";
@@ -22,6 +23,12 @@
 	};
 
 	let initialProj: I2D[] = [];
+
+	const defaultParams = {
+		numNeighborsUMAP: 15,
+		minDistUMAP: 0.1,
+	};
+	let { minDistUMAP, numNeighborsUMAP } = defaultParams;
 
 	$: data = initialProj.map((d, i) => {
 		return {
@@ -58,4 +65,42 @@
 		on:lasso={({ detail }) => {
 			console.log(detail);
 		}} />
+
+	<!-- Whenever I'm not filtering, go back to default view -->
+	<!-- whenever I'm filtering I can click visualize to reproject -->
+	<Button
+		variant="outlined"
+		on:click={() => {
+			console.log("visualized pressed");
+		}}>
+		Visualize
+	</Button>
+
+	<!-- UMAP parameter customization for later -->
+	<div>
+		<h3>UMAP Parameters</h3>
+		<div>
+			<label for="min-dist">Min Dist</label>
+			<input
+				id="min-dist"
+				bind:value={minDistUMAP}
+				type="range"
+				step="0.1"
+				min="0.1"
+				max="3.0" />
+			{minDistUMAP}
+		</div>
+
+		<div>
+			<label for="nn">Num Neighbors</label>
+			<input
+				id="nn"
+				type="range"
+				bind:value={numNeighborsUMAP}
+				step="10"
+				min="5"
+				max="100" />
+			{numNeighborsUMAP}
+		</div>
+	</div>
 </div>
