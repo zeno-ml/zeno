6d1b15d819eb836f3d07ff897038cf37be9347b8
xnought <bertuccd@oregonstate.edu>
2022-10-02 22:12:22 -0700

    fix: lasso selecting text

--- frontend/src/discovery/Discovery.svelte
+++ frontend/src/discovery/Discovery.svelte
@@ -38,5 +38,11 @@
 <style>
 	main {
 		display: flex;
+		-webkit-touch-callout: none; /* iOS Safari */
+		-webkit-user-select: none; /* Safari */
+		-khtml-user-select: none; /* Konqueror HTML */
+		-moz-user-select: none; /* Old versions of Firefox */
+		-ms-user-select: none; /* Internet Explorer/Edge */
+		user-select: none;
 	}
 </style>
