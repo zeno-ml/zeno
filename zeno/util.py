10f2cce266d3186bca78c9dd872434c2df2a100d
Donny Bertucci <bertuccd@oregonstate.edu>
2023-01-26 14:19:09 -0600

    fix: error when IPython module doesn't exist (#508)
    * fix: error when IPython module doesn't exist
    
    * fix: add NameError too  to be safe

--- zeno/util.py
+++ zeno/util.py
@@ -115,5 +115,5 @@ def is_notebook() -> bool:
             return False  # Terminal running IPython
         else:
             return False  # Other type (?)
-    except NameError:
+    except (NameError, ImportError):
         return False  # Probably standard Python interpreter
