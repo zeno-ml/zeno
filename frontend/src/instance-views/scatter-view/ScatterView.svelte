d131ca687e6aecdbd5db72e6463961b5b95f3eb5
xnought <bertuccd@oregonstate.edu>
2023-02-05 20:25:49 -0800

    refactor: format comment correctly!

--- frontend/src/instance-views/scatter-view/ScatterView.svelte
+++ frontend/src/instance-views/scatter-view/ScatterView.svelte
@@ -310,7 +310,6 @@
 		-khtml-user-select: none; /* Konqueror HTML */
 		-moz-user-select: none; /* Old versions of Firefox */
 		-ms-user-select: none; /* Internet Explorer/Edge */
-		user-select: none; /* Non-prefixed version, currently
-									supported by Chrome, Edge, Opera and Firefox */
+		user-select: none; /* Non-prefixed version, currently supported by Chrome, Edge, Opera and Firefox */
 	}
 </style>
