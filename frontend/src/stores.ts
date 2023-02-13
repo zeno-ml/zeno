e9acc09b396c563f9ef6205138df640eca9f36f7
Donny Bertucci <bertuccd@oregonstate.edu>
2023-02-13 10:22:41 -0800

    fix: colorByColumn doesn't get reset when remounted (#584)
    * fix: color selection doesn't reset on view change
    
    * feat: color other dtype as grey
    
    * refactor: remove 'global' from store name

--- frontend/src/stores.ts
+++ frontend/src/stores.ts
@@ -134,3 +134,5 @@ export const metricRangeColorScale: Readable<(n: number) => string> = derived(
 		return (n) => colorScale((n - min) / (max - min));
 	}
 );
+
+export const scatterColorByColumn: Writable<ZenoColumn> = writable(null);
