b6e8bf94eb8dd45cbcaa2c53349a46722d20f1bc
stevenyh3 <89871868+stevenyh3@users.noreply.github.com>
2023-02-16 23:50:54 +0800

    feat: add beeswarm plot option for report (#601)
    * feat: add pageholder for beeswarm chart
    
    * feat: finish beeswarm chart using slice size as dot size
    
    * refactor: remove unused variable
    
    * refactor: change ladybug icon to bee icon and report data namings
    
    * refactor: modify coloring, min size of plots and tooltip

---
+++ frontend/src/report/beeswarm-chart/BeeswarmChartReport.svelte
@@ -0,0 +1,84 @@
+<script lang="ts">
+	import Select, { Option } from "@smui/select";
+	import { Vega } from "svelte-vega";
+	import { getMetricsForSlices } from "../../api/slice";
+	import {
+		metric,
+		metrics,
+		model,
+		models,
+		reports,
+		slices,
+	} from "../../stores";
+	import type { MetricKey } from "../../zenoservice";
+	import generateSpec from "./vegaSpec-beeswarm";
+	export let reportId: number;
+
+	let chartEntries = [];
+
+	$: report = $reports[reportId];
+
+	function getMetKeys(rep, $metric, $model) {
+		const entries: MetricKey[] = [];
+		chartEntries = [];
+		rep.reportPredicates.forEach((pred) => {
+			chartEntries.push({
+				slice: pred.sliceName,
+				model: $model,
+			});
+			entries.push({
+				sli: [...$slices.values()].find((s) => s.sliceName === pred.sliceName),
+				metric: $metric,
+				model: $model,
+			});
+		});
+		return entries;
+	}
+
+	$: modelResults = getMetricsForSlices(getMetKeys(report, $metric, $model));
+</script>
+
+<div style:margin-left="20px">
+	<h4
+		contenteditable="true"
+		style:margin-right="20px"
+		style:padding="10px"
+		style:width="fit-content"
+		bind:textContent={$reports[reportId].name}>
+		{report.name}
+	</h4>
+
+	{#if $models && $models.length > 0}
+		<Select
+			bind:value={$model}
+			label="Model"
+			style="margin-right: 20px; width: fit-content">
+			{#each $models as m}
+				<Option value={m}>{m}</Option>
+			{/each}
+		</Select>
+	{/if}
+	{#if $metrics && $metrics.length > 0}
+		<Select
+			bind:value={$metric}
+			label="Metric"
+			style="margin-right: 20px; width: fit-content">
+			{#each $metrics as m}
+				<Option value={m}>{m}</Option>
+			{/each}
+		</Select>
+	{/if}
+	<br />
+	<div style:margin-top="30px" style:width="500px">
+		{#await modelResults then res}
+			{@const data = {
+				table: chartEntries.map((r, i) => ({
+					sli_name: r.slice,
+					size: res[i].size,
+					metric: res[i].metric.toFixed(2),
+				})),
+			}}
+			<Vega {data} spec={generateSpec($metric)} />
+		{/await}
+	</div>
+</div>
