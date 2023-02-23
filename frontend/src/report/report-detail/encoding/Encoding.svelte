67cbc287552c45d12a66b29200b5f0339e3dbef3
stevenyh3 <89871868+stevenyh3@users.noreply.github.com>
2023-02-23 11:44:21 -0500

    feat: add slice chips

--- frontend/src/report/report-detail/encoding/Encoding.svelte
+++ frontend/src/report/report-detail/encoding/Encoding.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
-	import { ready } from "../../../stores";
+	import { ready, report, reports } from "../../../stores";
 	import Select, { Option } from "@smui/select";
+	import { TrailingIcon } from "@smui/chips";
+	$: rep = $reports[$report];
 </script>
 
 {#if $ready}
@@ -9,13 +11,32 @@
 		<div id="encoding-flex">
 			<div class="parameters">
 				<h4 class="select-label">x</h4>
-				<Select class="select" variant="outlined">
+				<Select value="slice" class="select" variant="outlined">
 					<Option value={"slice"}>slice</Option>
+					<Option value={"model"}>model</Option>
 				</Select>
+			</div>
+			<div class="chips">
+				{#each rep.reportPredicates as predicate, i}
+					<div class="slice-chip">
+						{predicate.sliceName}
+						<TrailingIcon
+							class="remove material-icons"
+							on:click={() => {
+								rep.reportPredicates.splice(i, 1);
+								reports.update((reps) => {
+									reps[$report] = rep;
+									return reps;
+								});
+							}}>
+							cancel
+						</TrailingIcon>
+					</div>
+				{/each}
 			</div>
 			<div class="parameters">
 				<h4 class="select-label">y</h4>
-				<Select class="select" variant="outlined">
+				<Select value="accuracy" class="select" variant="outlined">
 					<Option value={"accuracy"}>accuracy</Option>
 				</Select>
 			</div>
@@ -56,5 +77,16 @@
 	}
 	.select-label {
 		margin: 5px;
+	}
+	.chips {
+		display: flex;
+		flex-wrap: wrap;
+		align-items: center;
+	}
+	.slice-chip {
+		padding: 5px 10px;
+		background: var(--P3);
+		margin: 5px;
+		border-radius: 10px;
 	}
 </style>
