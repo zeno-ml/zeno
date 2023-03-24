2e6da3f97dc677a8088b9cc89f4160e682c81586
stevenyh3 <89871868+stevenyh3@users.noreply.github.com>
2023-03-24 17:11:49 -0400

    refactor: redo radar chart

--- frontend/src/report/radar-chart/RadarChartReport.svelte
+++ frontend/src/report/radar-chart/RadarChartReport.svelte
@@ -3,31 +3,82 @@
 	import { getMetricsForSlices } from "../../api/slice";
 	import { report, reports } from "../../stores";
 	import type { MetricKey } from "../../zenoservice";
-	import generateSpec from "./vegaSpec-radar";
+	import { generateSpec } from "./vegaSpec-radar";
 
 	$: currentReport = $reports[$report];
-	$: selectMetrics = currentReport.metrics;
 	$: parameters = currentReport.parameters;
 
 	let chartEntries = [];
+	let fixedName = "";
 
 	function getMetKeys(rep) {
 		const metricKeys: MetricKey[] = [];
 		chartEntries = [];
 
-		rep.slices.forEach((slice) => {
-			rep.models.forEach((mod) => {
-				chartEntries.push({
-					slice: slice.sliceName,
-					model: mod,
+		if (parameters.xEncoding === "metrics") {
+			if (parameters.yEncoding === "slices") {
+				fixedName = rep.models[0];
+				rep.metrics.forEach((metric) => {
+					rep.slices.forEach((slice) => {
+						chartEntries.push({
+							key: metric,
+							category: slice.sliceName,
+						});
+						metricKeys.push({
+							sli: slice,
+							metric: metric,
+							model: rep.models[0],
+						});
+					});
 				});
-				metricKeys.push({
-					sli: slice,
-					metric: rep.metrics,
-					model: mod,
+			} else if (parameters.yEncoding === "models") {
+				fixedName = rep.slices[0].sliceName;
+				rep.metrics.forEach((metric) => {
+					rep.models.forEach((mod) => {
+						chartEntries.push({
+							key: metric,
+							category: mod,
+						});
+						metricKeys.push({
+							sli: rep.slices[0],
+							metric: metric,
+							model: mod,
+						});
+					});
 				});
-			});
-		});
+			}
+		} else if (parameters.colorEncoding === "metrics") {
+			fixedName = rep.metrics[0];
+			if (parameters.yEncoding === "slices") {
+				rep.models.forEach((mod) => {
+					rep.slices.forEach((slice) => {
+						chartEntries.push({
+							key: mod,
+							category: slice.sliceName,
+						});
+						metricKeys.push({
+							sli: slice,
+							metric: rep.metrics[0],
+							model: mod,
+						});
+					});
+				});
+			} else if (parameters.yEncoding === "models") {
+				rep.models.forEach((mod) => {
+					rep.slices.forEach((slice) => {
+						chartEntries.push({
+							key: slice.sliceName,
+							category: mod,
+						});
+						metricKeys.push({
+							sli: slice,
+							metric: rep.metrics[0],
+							model: mod,
+						});
+					});
+				});
+			}
+		}
 		return metricKeys;
 	}
 
@@ -39,16 +90,16 @@
 		{#await modelResults then res}
 			{@const chartData = {
 				table: chartEntries.map((r, i) => ({
-					slices: r.slice,
-					models: r.model,
-					metrics: res[i].metric.toFixed(2),
+					key: r.key,
+					value: res[i].metric.toFixed(2),
+					category: r.category,
 				})),
 			}}
-			<h4>{selectMetrics}</h4>
+			<h4>{fixedName}</h4>
 			<Vega
-				spec={generateSpec(parameters)}
+				spec={generateSpec}
 				data={chartData}
-				options={{ tooltip: true, width: 700, height: 600 }} />
+				options={{ tooltip: true, width: 800, height: 700 }} />
 		{/await}
 	</div>
 </div>
--- frontend/src/report/radar-chart/vegaSpec-radar.ts
+++ frontend/src/report/radar-chart/vegaSpec-radar.ts
@@ -1,257 +1,219 @@
 import type { VisualizationSpec } from "vega-embed";
 
-export default function generateSpec(parameters): VisualizationSpec {
-	const x_encode = parameters.xEncoding;
-	const y_encode = parameters.yEncoding;
-	const color_encode = parameters.colorEncoding;
-	const datum_x = "datum." + x_encode;
-	const datum_y = "datum." + y_encode;
-	const datum_color = "datum." + color_encode;
+export const generateSpec: VisualizationSpec = {
+	$schema: "https://vega.github.io/schema/vega/v5.json",
+	description:
+		"A radar chart example, showing multiple dimensions in a radial layout.",
+	autosize: { type: "none", contains: "padding" },
+	padding: { left: 80, right: 60, top: 60, bottom: 100 },
 
-	const spec = {
-		$schema: "https://vega.github.io/schema/vega/v5.json",
-		description:
-			"A radar chart example, showing multiple dimensions in a radial layout.",
-		autosize: { type: "none", contains: "padding" },
-		padding: { left: 80, right: 60, top: 60, bottom: 100 },
+	signals: [
+		{ name: "radius", update: "width / 2.3" },
+		{
+			name: "hover",
+			value: {},
+			on: [
+				{
+					events: "@category-line:mouseover",
+					update: "datum.category",
+				},
+				{
+					events: "@category-line:mouseout",
+					update: "{}",
+				},
+				{
+					events: "@value-text:mouseover",
+					update: "datum.datum.category",
+				},
+				{
+					events: "@value-text:mouseout",
+					update: "{}",
+				},
+			],
+		},
+	],
 
-		signals: [
-			{ name: "radius", update: "width / 2.3" },
-			{
-				name: "hover",
-				value: {},
-				on: [
-					{
-						events: "@category-line:mouseover",
-						update: datum_color,
-					},
-					{
-						events: "@category-line:mouseout",
-						update: "{}",
-					},
-					{
-						events: "@value-text:mouseover",
-						update: "datum." + datum_color,
-					},
-					{
-						events: "@value-text:mouseout",
-						update: "{}",
-					},
-				],
-			},
-		],
+	data: [
+		{ name: "table" },
+		{
+			name: "points",
+			source: "table",
+			transform: [
+				{
+					type: "aggregate",
+					groupby: ["key"],
+				},
+			],
+		},
+	],
 
-		data: [
-			{ name: "table" },
-			{
-				name: "points",
-				source: "table",
-				transform: [
-					{
-						type: "aggregate",
-						groupby: [x_encode],
-					},
-				],
-			},
-		],
-
-		scales: [
-			{
-				name: "angular",
-				type: "point",
-				range: { signal: "[-PI, PI]" },
-				padding: 0.5,
-				domain: { data: "table", field: x_encode },
-			},
-			{
-				name: "radial",
-				type: "linear",
-				range: { signal: "[0, radius]" },
-				zero: true,
-				nice: false,
-				domain: { data: "table", field: y_encode },
-				domainMin: 0,
-			},
-			{
-				name: "color",
-				type: "ordinal",
-				domain: { data: "table", field: color_encode },
-				range: { scheme: "category10" },
-			},
-		],
-		legends: [
-			{
-				type: "symbol",
-				title: "Color",
-				fill: "color",
-				direction: "horizontal",
-				orient: "top",
-				offset: 130,
-				titleFontSize: 12,
-				labelFontSize: 12,
-			},
-		],
-		encode: {
-			enter: {
-				x: { signal: "radius" },
-				y: { signal: "radius" },
+	scales: [
+		{
+			name: "angular",
+			type: "point",
+			range: { signal: "[-PI, PI]" },
+			padding: 0.5,
+			domain: { data: "table", field: "key" },
+		},
+		{
+			name: "radial",
+			type: "linear",
+			range: { signal: "[0, radius]" },
+			zero: true,
+			nice: false,
+			domain: { data: "table", field: "value" },
+			domainMin: 0,
+			domainMax: 100,
+		},
+		{
+			name: "color",
+			type: "ordinal",
+			domain: { data: "table", field: "category" },
+			range: { scheme: "category10" },
+		},
+	],
+	legends: [
+		{
+			fill: "color",
+			orient: "none",
+			title: "Layer",
+			encode: {
+				legend: { update: { x: { value: 300 }, y: { value: -250 } } },
 			},
 		},
+	],
+	encode: {
+		enter: {
+			x: { signal: "radius" },
+			y: { signal: "radius" },
+		},
+	},
 
-		marks: [
-			{
-				type: "group",
-				name: "categories",
-				zindex: 0.5,
-				from: {
-					facet: { data: "table", name: "facet", groupby: [color_encode] },
-				},
-				marks: [
-					{
-						type: "line",
-						name: "category-line",
-						from: { data: "facet" },
-						encode: {
-							enter: {
-								interpolate: { value: "linear-closed" },
-								x: {
-									signal:
-										"scale('radial'," +
-										datum_y +
-										")*cos(scale('angular'," +
-										datum_x +
-										"))",
-								},
-								y: {
-									signal:
-										"scale('radial'," +
-										datum_y +
-										")*sin(scale('angular'," +
-										datum_x +
-										"))",
-								},
+	marks: [
+		{
+			type: "group",
+			name: "categories",
+			zindex: 0.5,
+			from: {
+				facet: { data: "table", name: "facet", groupby: ["category"] },
+			},
+			marks: [
+				{
+					type: "line",
+					name: "category-line",
+					from: { data: "facet" },
+					encode: {
+						enter: {
+							interpolate: { value: "linear-closed" },
+							x: {
+								signal:
+									"scale('radial', datum.value) * cos(scale('angular', datum.key))",
 							},
-							update: {
-								stroke: [
-									{ test: datum_color + "=== hover", value: "black" },
-									{ scale: "color", field: color_encode },
-								],
-								strokeWidth: [
-									{ test: datum_color + "=== hover", value: 2 },
-									{ value: 1 },
-								],
-								strokeOpacity: [
-									{ test: datum_color + "=== hover", value: 0.8 },
-									{ value: 0.4 },
-								],
-								fill: { scale: "color", field: color_encode },
-								fillOpacity: { value: 0.2 },
+							y: {
+								signal:
+									"scale('radial', datum.value) * sin(scale('angular', datum.key))",
 							},
 						},
-					},
-					{
-						type: "text",
-						name: "value-text",
-						from: { data: "category-line" },
-						encode: {
-							enter: {
-								x: { field: "x" },
-								y: { field: "y" },
-							},
-							update: {
-								align: { value: "center" },
-								baseline: { value: "middle" },
-								text: { signal: "datum." + datum_y },
-								fill: { value: "black" },
-								fontSize: [
-									{ test: "datum." + datum_color + "=== hover", value: 13 },
-									{ value: 12 },
-								],
-								fontWeight: { value: "bold" },
-								fillOpacity: [
-									{ test: "datum." + datum_color + "=== hover", value: 1 },
-									{ value: 0.4 },
-								],
-							},
+						update: {
+							stroke: [
+								{ test: "datum.category === hover", value: "black" },
+								{ scale: "color", field: "category" },
+							],
+							strokeWidth: [
+								{ test: "datum.category === hover", value: 2 },
+								{ value: 1 },
+							],
+							strokeOpacity: [
+								{ test: "datum.category === hover", value: 0.8 },
+								{ value: 0.4 },
+							],
+							fill: { scale: "color", field: "category" },
+							fillOpacity: { value: 0.2 },
 						},
 					},
-				],
-			},
-			{
-				type: "rule",
-				name: "radial-grid",
-				from: { data: "points" },
-				zindex: 0,
-				encode: {
-					enter: {
-						x: { value: 0 },
-						y: { value: 0 },
-						x2: { signal: "radius * cos(scale('angular', " + datum_x + "))" },
-						y2: { signal: "radius * sin(scale('angular', " + datum_x + "))" },
-						stroke: { value: "lightgray" },
-						strokeWidth: { value: 2 },
-						strokeOpacity: { value: 0.7 },
-					},
 				},
-			},
-			{
-				type: "text",
-				name: "key-label",
-				from: { data: "points" },
-				zindex: 1,
-				encode: {
-					enter: {
-						x: {
-							signal: "(radius + 15) * cos(scale('angular', " + datum_x + "))",
+				{
+					type: "text",
+					name: "value-text",
+					from: { data: "category-line" },
+					encode: {
+						enter: {
+							x: { signal: "datum.x" },
+							y: { signal: "datum.y" },
 						},
-						y: {
-							signal: "(radius + 5) * sin(scale('angular', " + datum_x + "))",
+						update: {
+							align: { value: "center" },
+							baseline: { value: "middle" },
+							text: { signal: "datum.datum.value" },
+							fill: { value: "black" },
+							fontSize: [
+								{ test: "datum.datum.category === hover", value: 13 },
+								{ value: 12 },
+							],
+							fontWeight: { value: "bold" },
+							fillOpacity: [
+								{ test: "datum.datum.category === hover", value: 1 },
+								{ value: 0.4 },
+							],
 						},
-						text: { field: x_encode },
-						align: [
-							{
-								test: "abs(scale('angular', " + datum_x + ")) > PI / 2",
-								value: "right",
-							},
-							{
-								value: "left",
-							},
-						],
-						baseline: [
-							{
-								test: "scale('angular', " + datum_x + ") > 0",
-								value: "top",
-							},
-							{
-								test: "scale('angular', " + datum_x + ") == 0",
-								value: "middle",
-							},
-							{
-								value: "bottom",
-							},
-						],
-						fill: { value: "black" },
-						fontSize: { value: 12 },
-						fontWeight: { value: "bold" },
 					},
 				},
-			},
-			{
-				type: "line",
-				name: "outer-line",
-				from: { data: "radial-grid" },
-				encode: {
-					enter: {
-						interpolate: { value: "linear-closed" },
-						x: { field: "x2" },
-						y: { field: "y2" },
-						stroke: { value: "lightgray" },
-						strokeWidth: { value: 2 },
-						strokeOpacity: { value: 0.7 },
-					},
+			],
+		},
+		{
+			type: "rule",
+			name: "radial-grid",
+			from: { data: "points" },
+			zindex: 0,
+			encode: {
+				enter: {
+					x: { value: 0 },
+					y: { value: 0 },
+					x2: { signal: "radius * cos(scale('angular', datum.key))" },
+					y2: { signal: "radius * sin(scale('angular', datum.key))" },
+					stroke: { value: "gray" },
+					strokeWidth: { value: 2 },
+					strokeOpacity: { value: 0.2 },
 				},
 			},
-		],
-	};
-	return spec as VisualizationSpec;
-}
+		},
+		{
+			type: "text",
+			name: "key-label",
+			from: { data: "points" },
+			zindex: 1,
+			encode: {
+				enter: {
+					x: { signal: "(radius + 5) * cos(scale('angular', datum.key))" },
+					y: { signal: "(radius + 5) * sin(scale('angular', datum.key))" },
+					text: { field: "key" },
+					align: [
+						{
+							test: "abs(scale('angular', datum.key)) > PI / 2",
+							value: "right",
+						},
+						{
+							value: "left",
+						},
+					],
+					baseline: [
+						{
+							test: "scale('angular', datum.key) > 0",
+							value: "top",
+						},
+						{
+							test: "scale('angular', datum.key) == 0",
+							value: "middle",
+						},
+						{
+							value: "bottom",
+						},
+					],
+					fill: { value: "black" },
+					fontSize: { value: 12 },
+					fontWeight: { value: "bold" },
+				},
+			},
+		},
+	],
+};
