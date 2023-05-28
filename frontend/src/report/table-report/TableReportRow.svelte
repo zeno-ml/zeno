29bfd563984131d4bce5474f4c1b94fe666096bf
stevenyh3 <89871868+stevenyh3@users.noreply.github.com>
2023-05-28 01:18:08 -0400

    refactor the report table view

--- frontend/src/report/table-report/TableReportRow.svelte
+++ frontend/src/report/table-report/TableReportRow.svelte
@@ -1,105 +1,47 @@
 <script lang="ts">
 	import { Cell, Row } from "@smui/data-table";
-	import { getMetricsForSlices } from "../../api/slice";
-	import type { MetricKey } from "../../zenoservice";
 	import SliceDetailsContainer from "./SliceDetailsContainer.svelte";
 	import { slices } from "../../stores";
 
-	export let row: string;
-	export let fixed_dimension;
+	export let yCell: string;
 	export let parameters;
 	export let currentReport;
-
-	$: modelResults = getMetricsForSlices(getMetKeys(currentReport));
-
-	function getMetKeys(rep) {
-		const metricKeys: MetricKey[] = [];
-		if (fixed_dimension === "z") {
-			if (parameters.yEncoding === "slices") {
-				rep.models.forEach((mod) => {
-					metricKeys.push({
-						sli: $slices.get(row),
-						metric: rep.metrics[0],
-						model: mod,
-					});
-				});
-			} else if (parameters.yEncoding === "models") {
-				rep.slices.forEach((sli) => {
-					metricKeys.push({
-						sli: $slices.get(sli),
-						metric: rep.metrics[0],
-						model: row,
-					});
-				});
-			}
-		} else if (fixed_dimension === "y") {
-			if (parameters.yEncoding === "slices") {
-				rep.models.forEach((mod) => {
-					metricKeys.push({
-						sli: $slices.get(rep.slices[0]),
-						metric: row,
-						model: mod,
-					});
-				});
-			} else if (parameters.yEncoding === "models") {
-				rep.slices.forEach((sli) => {
-					metricKeys.push({
-						sli: $slices.get(sli),
-						metric: row,
-						model: rep.models[0],
-					});
-				});
-			}
-		}
-		return metricKeys;
-	}
+	export let result;
 </script>
 
 <Row style="overflow: visible">
-	{#if fixed_dimension === "z"}
-		<Cell class="sticky" style="left: 0px; border-right: 1px solid #e8e8e8">
-			<div class="inline">
-				{#if parameters.yEncoding === "slices"}
-					<SliceDetailsContainer sli={$slices.get(row)} />
-				{:else if parameters.yEncoding === "models"}
-					{row}
-				{/if}
-			</div>
-		</Cell>
-		<Cell>{currentReport.metrics[0]}</Cell>
-	{:else if fixed_dimension === "y"}
-		<Cell class="sticky" style="left: 0px; border-right: 1px solid #e8e8e8">
-			<div class="inline">
-				{#if parameters.yEncoding === "slices"}
-					<SliceDetailsContainer sli={$slices.get(currentReport.slices[0])} />
-				{:else if parameters.yEncoding === "models"}
-					{currentReport.models[0]}
-				{/if}
-			</div>
-		</Cell>
-		<Cell>{row}</Cell>
-	{/if}
-	{#await modelResults then res}
-		{#if res}
-			{#each res as r}
-				<Cell>
-					<p>
-						{row === "size" ||
-						(fixed_dimension === "z" && currentReport.metrics[0] === "size")
-							? r.size
-							: r.metric.toFixed(2)}
-					</p>
-				</Cell>
-			{/each}
+	<!-- fixed cell -->
+	<Cell class="sticky" style="left: 0px; border-right: 1px solid #e8e8e8;">
+		{#if parameters.zEncoding === "slices"}
+			<SliceDetailsContainer sli={$slices.get(currentReport.slices[0])} />
+		{:else if parameters.zEncoding === "models"}
+			{currentReport.models[0]}
+		{:else if parameters.zEncoding === "metrics"}
+			{currentReport.metrics[0]}
 		{/if}
-	{/await}
+	</Cell>
+	<!-- y cell -->
+	<Cell>
+		{#if parameters.yEncoding === "slices"}
+			<SliceDetailsContainer sli={$slices.get(yCell)} />
+		{:else if parameters.yEncoding === "models"}
+			{yCell}
+		{/if}
+	</Cell>
+	{#each result as r}
+		<Cell style="text-align: center;">
+			<p>
+				{!r.metric ||
+				(parameters.zEncoding === "metrics" &&
+					currentReport.metrics[0] === "size")
+					? r.size
+					: r.metric.toFixed(2)}
+			</p>
+		</Cell>
+	{/each}
 </Row>
 
 <style>
-	.inline {
-		display: flex;
-		align-items: center;
-	}
 	:global(.sticky) {
 		overflow: visible;
 		left: 0px;
--- frontend/src/report/table-report/TableReportTable.svelte
+++ frontend/src/report/table-report/TableReportTable.svelte
@@ -3,71 +3,127 @@
 	import DataTable, { Body, Cell, Head, Row } from "@smui/data-table";
 	import SliceDetailsContainer from "./SliceDetailsContainer.svelte";
 	import TableReportRow from "./TableReportRow.svelte";
-
-	let table: HTMLDivElement;
+	import { getMetricsForSlices } from "../../api/slice";
+	import type { MetricKey } from "../../zenoservice";
 
 	$: currentReport = $reports[$report];
 	$: selectModels = currentReport.models;
 	$: selectMetrics = currentReport.metrics;
 	$: selectSlices = currentReport.slices;
 	$: parameters = currentReport.parameters;
-	$: fixed_dimension = currentReport.parameters.fixedDimension;
+
+	function getMetKeys(rep) {
+		const metricKeys: MetricKey[] = [];
+		// fixed metrics
+		if (parameters.zEncoding === "metrics") {
+			// x: models, y: slices
+			if (parameters.yEncoding === "slices") {
+				rep.slices.forEach((sli) => {
+					rep.models.forEach((mod) => {
+						metricKeys.push({
+							sli: $slices.get(sli),
+							metric: rep.metrics[0],
+							model: mod,
+						});
+					});
+				});
+			}
+			// x: slices, y: models
+			else if (parameters.yEncoding === "models") {
+				rep.models.forEach((mod) => {
+					rep.slices.forEach((sli) => {
+						metricKeys.push({
+							sli: $slices.get(sli),
+							metric: rep.metrics[0],
+							model: mod,
+						});
+					});
+				});
+			}
+		}
+		// fixed slices
+		else if (parameters.zEncoding === "slices") {
+			// x: metrics, y: models
+			rep.models.forEach((mod) => {
+				rep.metrics.forEach((met) => {
+					metricKeys.push({
+						sli: $slices.get(rep.slices[0]),
+						metric: met,
+						model: mod,
+					});
+				});
+			});
+		}
+		// fixed models
+		else if (parameters.zEncoding === "models") {
+			// x: metrics, y: slices
+			rep.slices.forEach((sli) => {
+				rep.metrics.forEach((met) => {
+					metricKeys.push({
+						sli: $slices.get(sli),
+						metric: met,
+						model: rep.models[0],
+					});
+				});
+			});
+		}
+		return metricKeys;
+	}
+	$: modelResults = getMetricsForSlices(getMetKeys(currentReport));
 </script>
 
 <div id="container">
-	<div bind:this={table}>
+	<div>
 		<DataTable style="max-width: calc(100vw - 450px);">
 			<Head>
 				<Row>
 					<Cell class="sticky" style="border-right: 1px solid #e8e8e8">
-						{parameters.yEncoding}
+						fixed
 					</Cell>
-					<Cell>{parameters.zEncoding}</Cell>
+					<Cell>{parameters.yEncoding} \ {parameters.xEncoding}</Cell>
 					{#if parameters.xEncoding === "slices"}
 						{#each selectSlices as slice}
-							<Cell>
-								<div class="inline">
-									{#if slice}
-										<SliceDetailsContainer sli={$slices.get(slice)} />
-									{/if}
-								</div>
+							<Cell style="width: 120px; max-width: 120px; text-align: center;">
+								<SliceDetailsContainer sli={$slices.get(slice)} />
 							</Cell>
 						{/each}
 					{:else if parameters.xEncoding === "models"}
 						{#each selectModels as m}
-							<Cell>{m}</Cell>
+							<Cell style="width: 120px; max-width: 120px; text-align: center;">
+								{m}
+							</Cell>
+						{/each}
+					{:else if parameters.xEncoding === "metrics"}
+						{#each selectMetrics as m}
+							<Cell style="width: 120px; max-width: 120px; text-align: center;">
+								{m}
+							</Cell>
 						{/each}
 					{/if}
 				</Row>
 			</Head>
 			<Body style="overflow: visible">
-				{#if fixed_dimension === "z"}
+				{#await modelResults then res}
 					{#if parameters.yEncoding === "slices"}
-						{#each selectSlices as slice}
+						{@const resultSize = res.length / selectSlices.length}
+						{#each selectSlices as slice, i}
 							<TableReportRow
-								row={slice}
-								{fixed_dimension}
+								result={res.slice(resultSize * i, resultSize * (i + 1))}
+								yCell={slice}
 								{parameters}
 								{currentReport} />
 						{/each}
 					{:else if parameters.yEncoding === "models"}
-						{#each selectModels as model}
+						{@const resultSize = res.length / selectModels.length}
+						{#each selectModels as model, i}
 							<TableReportRow
-								row={model}
-								{fixed_dimension}
+								result={res.slice(resultSize * i, resultSize * (i + 1))}
+								yCell={model}
 								{parameters}
 								{currentReport} />
 						{/each}
 					{/if}
-				{:else if fixed_dimension === "y"}
-					{#each selectMetrics as metric}
-						<TableReportRow
-							row={metric}
-							{fixed_dimension}
-							{parameters}
-							{currentReport} />
-					{/each}
-				{/if}
+				{/await}
 			</Body>
 		</DataTable>
 	</div>
@@ -77,9 +133,5 @@
 	#container {
 		margin-left: 20px;
 		margin-top: 20px;
-	}
-	.inline {
-		display: flex;
-		align-items: center;
 	}
 </style>
