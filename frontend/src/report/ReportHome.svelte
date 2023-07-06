16316b56251fbfd1da30d2e5e5d323bdac5eeff1
stevenyh3 <89871868+stevenyh3@users.noreply.github.com>
2023-07-05 22:00:37 -0400

    new table view for report home page

--- frontend/src/report/ReportHome.svelte
+++ frontend/src/report/ReportHome.svelte
@@ -1,25 +1,42 @@
 <script lang="ts">
 	import { mdiPlus } from "@mdi/js";
-	import { Svg } from "@smui/common";
-	import { Icon } from "@smui/icon-button";
+	import DataTable, {
+		Head,
+		Body,
+		Row,
+		Cell,
+		Pagination,
+	} from "@smui/data-table";
+	import { Label, Svg } from "@smui/common";
+	import Select, { Option } from "@smui/select";
+	import IconButton, { Icon } from "@smui/icon-button";
 	import { reports, slices, models, metrics } from "../stores";
-	import { updateTab } from "../util/util";
-	import ReportHomeBlock from "./ReportHomeBlock.svelte";
 	import { ChartType } from "../zenoservice";
+	import { updateTab } from "../util/util";
+	import ReportHomeRow from "./ReportHomeRow.svelte";
+
+	let rowsPerPage = 10;
+	let currentPage = 0;
+
+	let blur = function (ev) {
+		ev.target.blur();
+	};
+
+	$: start = currentPage * rowsPerPage;
+	$: end = Math.min(start + rowsPerPage, $reports.length);
+	$: content = $reports.slice(start, end);
+	$: lastPage = Math.max(Math.ceil($reports.length / rowsPerPage) - 1, 0);
+
+	$: if (currentPage > lastPage) {
+		currentPage = lastPage;
+	}
 </script>
 
 <div id="reports-container">
 	<div class="header">
 		<h3>Reports</h3>
-	</div>
-	<div class="reports">
-		{#each $reports as rep, i}
-			<ReportHomeBlock report={rep} reportIndex={i} />
-		{/each}
-		<div
-			class="add-reports"
-			on:click={(e) => {
-				e.stopPropagation();
+		<IconButton
+			on:click={() => {
 				reports.update((reps) => {
 					updateTab("report/" + reps.length + "/new");
 					reps.push({
@@ -39,13 +56,69 @@
 					return reps;
 				});
 			}}
-			on:keydown={() => ({})}>
-			<div class="add-button">
-				<Icon style="outline:none" component={Svg} viewBox="0 0 24 24">
-					<path fill="black" d={mdiPlus} />
-				</Icon>
-			</div>
-		</div>
+			on:mouseleave={blur}
+			on:focusout={blur}>
+			<Icon style="outline:none" component={Svg} viewBox="0 0 24 24">
+				<path fill="black" d={mdiPlus} />
+			</Icon>
+		</IconButton>
+	</div>
+	<div class="reports">
+		<DataTable style="width: 100%">
+			<Head>
+				<Row>
+					<Cell numeric>ID</Cell>
+					<Cell>Type</Cell>
+					<Cell style="width: 100%;">Name</Cell>
+					<Cell>Content</Cell>
+					<Cell>Options</Cell>
+				</Row>
+			</Head>
+			<Body>
+				{#each content as report, reportIndex}
+					<ReportHomeRow {report} {reportIndex} />
+				{/each}
+			</Body>
+
+			<Pagination slot="paginate">
+				<svelte:fragment slot="rowsPerPage">
+					<Label>Rows Per Page</Label>
+					<Select variant="outlined" bind:value={rowsPerPage} noLabel>
+						<Option value={5}>5</Option>
+						<Option value={10}>10</Option>
+						<Option value={25}>25</Option>
+					</Select>
+				</svelte:fragment>
+				<svelte:fragment slot="total">
+					{start + 1}-{end} of {$reports.length}
+				</svelte:fragment>
+
+				<IconButton
+					class="material-icons"
+					action="first-page"
+					title="First page"
+					on:click={() => (currentPage = 0)}
+					disabled={currentPage === 0}>first_page</IconButton>
+				<IconButton
+					class="material-icons"
+					action="prev-page"
+					title="Prev page"
+					on:click={() => currentPage--}
+					disabled={currentPage === 0}>chevron_left</IconButton>
+				<IconButton
+					class="material-icons"
+					action="next-page"
+					title="Next page"
+					on:click={() => currentPage++}
+					disabled={currentPage === lastPage}>chevron_right</IconButton>
+				<IconButton
+					class="material-icons"
+					action="last-page"
+					title="Last page"
+					on:click={() => (currentPage = lastPage)}
+					disabled={currentPage === lastPage}>last_page</IconButton>
+			</Pagination>
+		</DataTable>
 	</div>
 </div>
 
@@ -59,33 +132,10 @@
 	.reports {
 		display: flex;
 		flex-wrap: wrap;
+		width: calc(60vw);
 	}
 	.header {
 		display: flex;
-		justify-content: space-between;
 		align-items: center;
-	}
-	.add-reports {
-		display: flex;
-		flex-direction: column;
-		justify-content: space-around;
-		align-items: center;
-		border: 1px solid var(--G4);
-		border-radius: 10px;
-		margin: 5px 5px 5px 5px;
-		padding-left: 10px;
-		padding-right: 10px;
-		overflow: visible;
-		cursor: pointer;
-		width: 225px;
-		height: 100px;
-	}
-	.add-reports:hover {
-		background: #f0ebf4;
-	}
-	.add-button {
-		width: 24px;
-		height: 24px;
-		margin: 14px;
 	}
 </style>
---
+++ frontend/src/report/ReportHomeRow.svelte
@@ -0,0 +1,123 @@
+<script lang="ts">
+	import {
+		mdiBee,
+		mdiChartBar,
+		mdiChartLine,
+		mdiRadar,
+		mdiTable,
+		mdiViewGrid,
+	} from "@mdi/js";
+	import IconButton, { Icon } from "@smui/icon-button";
+	import Button from "@smui/button";
+	import { Row, Cell } from "@smui/data-table";
+	import { Svg } from "@smui/common";
+	import { reports } from "../stores";
+	import { ChartType } from "../zenoservice";
+	import { updateTab } from "../util/util";
+
+	export let report;
+	export let reportIndex;
+
+	let iconMap = {
+		[ChartType.TABLE]: mdiTable,
+		[ChartType.LINE]: mdiChartLine,
+		[ChartType.BAR]: mdiChartBar,
+		[ChartType.BEESWARM]: mdiBee,
+		[ChartType.RADAR]: mdiRadar,
+		[ChartType.HEATMAP]: mdiViewGrid,
+	};
+
+	let blur = function (ev) {
+		ev.target.blur();
+	};
+</script>
+
+<Row>
+	<Cell numeric>{reportIndex}</Cell>
+	<Cell>
+		<div class="report-type">
+			<Icon style="outline:none" component={Svg} viewBox="0 0 24 24">
+				<path fill="black" d={iconMap[report.type]} />
+			</Icon>
+		</div>
+	</Cell>
+	<Cell>
+		<div class="inline">
+			<p class="report-name">{report.name}</p>
+			<Button
+				style="background-color: white; height: 25px"
+				variant="outlined"
+				on:click={() => updateTab("report/" + reportIndex)}>
+				View
+			</Button>
+		</div>
+	</Cell>
+	<Cell>
+		<p class="report-slice">
+			{report.slices ? report.slices.length : 0} slices,
+			{report.models ? report.models.length : 0} models,
+			{report.metrics ? report.metrics.length : 0} metrics
+		</p>
+	</Cell>
+	<Cell>
+		<div class="inline">
+			<IconButton
+				on:click={() => {
+					reports.update((reps) => {
+						reps.push({
+							name: "Copy of " + report.name,
+							type: report.type,
+							slices: report.slices,
+							models: report.models,
+							metrics: report.metrics,
+							parameters: report.parameters,
+						});
+						return reps;
+					});
+				}}
+				on:mouseleave={blur}
+				on:focusout={blur}>
+				<Icon style="font-size: 20px;" class="material-icons">
+					content_copy
+				</Icon>
+			</IconButton>
+			<IconButton
+				on:click={() => {
+					reports.update((reps) => {
+						reps.splice(reportIndex, 1);
+						return reps;
+					});
+				}}
+				on:mouseleave={blur}
+				on:focusout={blur}>
+				<Icon style="font-size: 21px;" class="material-icons">
+					delete_outline
+				</Icon>
+			</IconButton>
+		</div>
+	</Cell>
+</Row>
+
+<style>
+	.inline {
+		display: flex;
+		align-items: center;
+	}
+	.report-type {
+		width: 24px;
+		height: 24px;
+		margin: 4px;
+	}
+	.report-name {
+		font-size: 14px;
+		color: black;
+		text-overflow: ellipsis;
+		overflow: hidden;
+		max-width: 100%;
+		white-space: nowrap;
+		margin-right: 10px;
+	}
+	.report-slice {
+		margin: 3px;
+	}
+</style>
